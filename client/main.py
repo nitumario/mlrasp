f0ab26951701feacfffd16f60d33de1ddcccd6aa
nitu adina <nitu.mario@gmail.com>
2023-05-06 01:36:32 +0300

    modified:   server/main.sh

--- client/main.py
+++ client/main.py
@@ -13,7 +13,7 @@ dt_string = now.strftime("%d/%m/%Y %H:%M:%S") #get the time and date
 print('you are uploading')
 hostname=socket.gethostname()   
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-clientSocket.connect(("127.0.0.1",9090)) #connect to server
+clientSocket.connect(("192.168.1.24",9090)) #connect to server
 data = "{ \n" + socket.gethostbyname(hostname) + "\n SCOPE:upload \n" + dt_string + "\n" + str(uuid.uuid1()) +  "\n }" # creates the header
 datasave = open("header.txt", "w")
 datasave.write("{ \n" + socket.gethostbyname(hostname) + "\n SCOPE:upload \n" + dt_string + "\n" + str(uuid.uuid1()) +  "\n }") #saves it in header.txt
--- server/main.sh
+++ server/main.sh
@@ -10,3 +10,4 @@ echo $passwd
 set -o noclobber
 printf "{ \n UUID: $uuid \n IP: $ip \n USR: $usr \n PASSWD: $passwd \n DATE: $(date +"%m %d %Y %H:%M:%S") \n }, \n" >> data.txt
 printf "{ \n UUID: $uuid \n IP: $ip \n USR: $usr \n PASSWD: $passwd \n DATE: $(date +"%m %d %Y %H:%M:%S") \n }, \n" >> tempdata.txt
+sudo chmod -R a+rwx tempdata.txt
