18044469daf5decc8538804a99124400e2675746
nitumario <nitu.mario@gmail.com>
2023-05-06 17:02:19 +0300

    modified:   server/init.sh 	new file:   server/reset.conf

--- server/init.sh
+++ server/init.sh
@@ -9,10 +9,7 @@ echo "3"
 sleep 1
 sudo ufw allow 20/tcp
 sudo ufw allow 21/tcp
+sudo ufw allow 22/tcp
 sudo ufw status
-sudo systemctl enable vsftpd.service
-sudo systemctl start vsftpd.service
-sudo systemctl restart vsftpd.service
-sudo systemctl status vsftpd.service
 echo "Starting main"
-sudo bash main.sh
+sudo python3 main.py
