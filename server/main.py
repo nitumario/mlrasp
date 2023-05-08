7e9372dddebbe60ef0f6d2014006cce3ef052fcd
nitumario <nitu.mario@gmail.com>
2023-05-08 15:59:01 +0300

    modified:   server/main.py

--- server/main.py
+++ server/main.py
@@ -1,14 +1,8 @@
 import os
 import socket
 import subprocess
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind(("192.168.1.24", 9090))
-server_socket.listen()
 
-while True:
-    (client_connected, client_address) = server_socket.accept()
-    print("Accepted a connection request from %s:%s" % (client_address[0], client_address[1]))
-    
+def handle_client(client_connected):
     # Receives the header
     data_from_client = client_connected.recv(1024)
     print(data_from_client.decode())
@@ -32,21 +26,39 @@ while True:
         print("PASSWD: ", PASSWD)
         USR = USR.strip()
         PASSWD = PASSWD.strip()
-    # create user and set home directory to /upload and login shell to /sbin/nologin
-    subprocess.run(["useradd", "-g", "sftp", "-d", "/incoming", "-s", "/sbin/nologin", USR])
+        
+        # create user and set home directory to /upload and login shell to /sbin/nologin
+        subprocess.run(["useradd", "-g", "sftp", "-d", "/incoming", "-s", "/sbin/nologin", USR])
 
-# set the password for the user
-    proc = subprocess.Popen(["passwd", USR], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    proc.stdin.write(f"{PASSWD}\n{PASSWD}\n".encode())
-    proc.communicate()
+        # set the password for the user
+        proc = subprocess.Popen(["passwd", USR], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        proc.stdin.write(f"{PASSWD}\n{PASSWD}\n".encode())
+        proc.communicate()
 
-# create incoming directory and set permissions
-    subprocess.run(["mkdir", "-p", f"/data/{USR}/incoming"])
-    subprocess.run(["chown", "-R", "root:sftp", f"/data/{USR}"])
-    subprocess.run(["chown", "-R", f"{USR}:sftp", f"/data/{USR}/incoming"])
+        # create incoming directory and set permissions
+        subprocess.run(["mkdir", "-p", f"/data/{USR}/incoming"])
+        subprocess.run(["chown", "-R", "root:sftp", f"/data/{USR}"])
+        subprocess.run(["chown", "-R", f"{USR}:sftp", f"/data/{USR}/incoming"])
 
-    with open("tempdata.txt", "r") as tempfile:
-        tempdata = " ".join(tempfile.readlines())
-        print("\n", tempdata)
-        client_connected.send(tempdata.encode())
-    os.system("sudo rm tempdata.txt")
+        with open("tempdata.txt", "r") as tempfile:
+            tempdata = " ".join(tempfile.readlines())
+            print("\n", tempdata)
+            client_connected.send(tempdata.encode())
+            
+        os.system("sudo rm tempdata.txt")
+
+def start_server(host, port):
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((host, port))
+    server_socket.listen()
+
+    while True:
+        (client_connected, client_address) = server_socket.accept()
+        print("Accepted a connection request from %s:%s" % (client_address[0], client_address[1]))
+        
+        handle_client(client_connected)
+        
+    server_socket.close()
+
+# example usage:
+start_server("192.168.1.24", 9090)
