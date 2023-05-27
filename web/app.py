bfd1e6848aaec24cf5f19a119dabbc41b4524e3d
Vlad-Andrei <65975497+Vlad-Andrei@users.noreply.github.com>
2023-05-27 10:07:25 +0300

    Update app.py

--- web/app.py
+++ web/app.py
@@ -28,31 +28,25 @@ def upload():
 @app.route('/success', methods=['POST'])
 def success():
     try:
-        # Get the current date and time
         now = datetime.now()
         dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
         print("You are uploading at", dt_string)
 
-        # Get the hostname and create a socket
         hostname = socket.gethostname()
         client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         client_socket.connect((ip, 9090))  # Connect to server
 
-        # Create the header data and save it to a file
         header_data = "{ \n" + socket.gethostbyname(hostname) + "\n SCOPE:upload \n" + dt_string + "\n" + str(uuid.uuid4()) + "\n}"
         with open("header.txt", "w") as datasave:
             datasave.write(header_data)
 
-        # Send the header to the server and receive a response
         client_socket.send(header_data.encode())
         data_from_server = client_socket.recv(1024).decode()
 
-        # Save the response to a file and print it
         with open("header_response.txt", "w") as datasave:
             datasave.write(data_from_server)
         print(data_from_server)
 
-        # Get the USR and PASSWD values from the header response
         with open("header_response.txt", "r") as header_file:
             header = header_file.read()
             usr = header.split("USR: ")[1].split("\n")[0].strip()
@@ -60,29 +54,22 @@ def success():
         os.system("sudo rm header_response.txt")
         os.system("sudo rm header.txt")
 
-        # Generate a random 6-digit code
         code = str(random.randint(100000, 999999))
 
-        # Create a new SQLite connection and cursor
         con = sqlite3.connect(db_file)
         cursor = con.cursor()
 
-        # Create the users table if it doesn't exist
         cursor.execute("CREATE TABLE IF NOT EXISTS users (username TEXT, password TEXT, code TEXT)")
 
-        # Insert user information into the database
         cursor.execute("INSERT INTO users (username, password, code) VALUES (?, ?, ?)", (usr, passwd, code))
         con.commit()
 
-        # Create a Paramiko SSH client
         ssh = paramiko.SSHClient()
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         ssh.connect(ip, username=usr, password=passwd)
 
-        # Create an SFTP client from the SSH client
         sftp = ssh.open_sftp()
 
-        # Loop through each uploaded file and upload it to the SFTP server
         for uploaded_file in request.files.getlist('file'):
             filename = uploaded_file.filename
             sftp.putfo(uploaded_file, '/incoming/' + filename)
@@ -90,7 +77,7 @@ def success():
         sftp.close()
         ssh.close()
 
-        con.close()  # Close the SQLite connection
+        con.close()
 
         return render_template('success.html', code=code)
 
@@ -120,15 +107,11 @@ def view():
             ssh = paramiko.SSHClient()
             ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
             ssh.connect(ip, username=username, password=password)
-            # Create an SFTP client from the SSH client
             sftp = ssh.open_sftp()
-            # Change to the desired directory on the SFTP server
             sftp.chdir("/incoming/")
 
-            # Get the list of files in the directory
             files = sftp.listdir_attr()
 
-            # Create a list of dictionaries containing file name and file path
             for file in files:
                 if S_ISDIR(file.st_mode):
                     folder_path = sftp.normalize('/incoming/' + file.filename)
@@ -140,7 +123,6 @@ def view():
                     file_path = sftp.normalize('/incoming/' + file.filename)
                     file_info.append({'file': file.filename, 'file_path': file_path})
 
-            # Close the SFTP session
             sftp.close()
         else:
             app.logger.info('Invalid code: {}'.format(code))
@@ -156,7 +138,6 @@ def view():
 
     except Exception as e:
         app.logger.error('An error occurred: {}'.format(str(e)))
-        # Handle the error and return an appropriate response
         return render_template('error.html', message="An error occurred")
 
     finally:
